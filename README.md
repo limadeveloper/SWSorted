f6a6783f823b3f6b15ffb656f62bec80da222088
John Lima <john.lima_desenv@outlook.com>
2016-11-08 11:56:55 -0200

    • Dictionaries; • .gitignore;

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Swift Sorted
 - Swift 3;
 - Xcode 8.1;
+- Array;
+- Dictionaries;
--- Sorted.playground/Contents.swift
+++ Sorted.playground/Contents.swift
@@ -3,14 +3,16 @@
 //  Created by John Lima on 08/11/16.
 //  Copyright (c) 2016 limadeveloper. All rights reserved.
 //
+
 typealias ArrayType = [Dictionary<String, Any>]
 typealias DictionaryType = Dictionary<String, Any>
 
 // ------------------------------------
 var array = ArrayType()
-var dictionary = DictionaryType()
+var dictionary1 = DictionaryType()
+var dictionary2 = DictionaryType()
 var menu: DictionaryType = ["12 - Main course": 10.99, "23 - Dessert": 2.99, "11 - Salad": 5.99]
-let sorted = menu.sorted(by: { $0.0 < $1.0 })
+let sorted = menu.sorted(by: { $0.key < $1.key })
 
 sorted
 
@@ -19,12 +21,17 @@ for (key, value) in sorted {
     let item = [key: value]
     array.append(item)
     
-    dictionary.updateValue(value, forKey: key)
+    dictionary1.updateValue(value, forKey: key)
+    
+    dictionary2[key] = value
 }
 
 array
 array.count
 
-dictionary
-dictionary.count
+dictionary1
+dictionary1.count
+
+dictionary2
+dictionary2.count
 
